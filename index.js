6582634711ee514b70d2a3f4b11de94e00c19083
OlenaTsapliuk <74963920+OlenaTsapliuk@users.noreply.github.com>
2021-11-26 13:26:25 +0200

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll(".slide");
+
+  slides[activeSlide].classList.add("active");
+  slides.forEach((slide) => {
+    slide.addEventListener("click", () => {
+      removeActiveClasses();
+      slide.classList.add("active");
+    });
+  });
+
+  function removeActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
+}
+
+slidesPlugin();
